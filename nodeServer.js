7453576bc158185a1b6fb3bb579820c16e61d167
ask2696 <ask_2696@gmail.com>
2017-06-23 00:29:07 +0530

    Added Send/Receive and Serving Html Pages

--- nodeServer.js
+++ nodeServer.js
@@ -1,6 +1,8 @@
 var net= require('net');
 var fs=require('fs');
+var formidable = require("formidable");
 var io = require('socket.io');
+//var io = require('socket.io');
 
 
 
@@ -15,6 +17,10 @@ var battery_current;
 var battery_temperature;
 var pod_temperature;
 var stripe_count;
+
+var no_data_packet =0;
+
+var input_base = "NULL";
 
 var dataJSON=new Object();
 dataJSON={ "team_id":'0',
@@ -51,6 +57,7 @@ function unpack8(arr)
                 		console.log("ended");
                 	});
 
+
                 	socket.on('data',function (data){
 
         				
@@ -75,14 +82,19 @@ function unpack8(arr)
                          arr = Array.prototype.slice.call(data, 26,30);
                         dataJSON.pod_temperature=unpack(arr);
                          arr = Array.prototype.slice.call(data,30,34);
-                        dataJSON.stripe_count=unpack(arr);    
+                        dataJSON.stripe_count=unpack(arr);
 
+                        no_data_packet = no_data_packet +1;
+                        //console.log(no_data_packet)
+                        console.log("Data Packet No."+ no_data_packet)
                         console.log(dataJSON); 	
+
+                        socket.write(input_base);
                      //   app.get('/data.html', browserdisp );
 	                     });
 
         });
-var data_to_pod = {"Test1":"1","Test2":"2"};
+/*var data_to_pod = {"Test1":"1","Test2":"2"};
 var listener = io.listen(server);
 listener.sockets.on('connection',function(socket_to_pod){
 
@@ -92,7 +104,7 @@ listener.sockets.on('connection',function(socket_to_pod){
    },0); 
 
 
-});
+});*/
 
    
 
@@ -106,17 +118,83 @@ server.listen(3000,function (){
 
 //Frontend - Webapp
 
-function browserdisp(req,resp){
+/*function browserdisp(req,resp){
                         resp.send(JSON.stringify(dataJSON));
                        // if(req){var start=1;}
                       
-                      }
+                      }*/
 
 var http = require("http");
 var express=require('express');
 var app=express();
 
+var server1 = http.createServer(app);
+     //app.listen(2000,()=>{console.log("2000 server")});
 
-     app.listen(2000,()=>{console.log("2000 server")});
+//app.get('/data.html', browserdisp );
 
-app.get('/data.html', browserdisp );
+app.get('/', function (req,res){
+
+    res.send("Base Station!");
+
+});
+
+app.get('/index.html',function(req,res){
+    
+
+    fs.readFile( __dirname + "/index.html",function(error,data){               
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    res.write(data, "utf8");
+                    res.end();
+                    });
+
+});
+
+app.post('/index.html',function(request,response){
+
+                    console.log("Got Post!!");
+                    var form = new formidable.IncomingForm();
+                    
+
+                    form.parse(request,function(err,field){
+
+                        // change input_newsong to LIST
+                        input = field;
+                        console.log(input.ping1);
+                        input_base = input.ping1;
+                        
+
+
+
+                    fs.readFile(__dirname + "/index.html",function(error,data){               
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.write(data, "utf8");
+                    response.end();
+                    });
+
+                     
+                    
+                    })
+
+                });
+
+
+var listener = io.listen(server1);
+listener.sockets.on('connection', function(socket){
+
+    socket.on('join', function(data_client) {
+        console.log(data_client);
+        console.log("Client Connected!!")
+    });
+
+   
+
+    setInterval(function(){
+        socket.emit('data_send', {'data_from_pod': dataJSON["team_id"]});
+    }, 0);
+
+});
+
+
+server1.listen(8081,"0.0.0.0");
+console.log("Server listening at 8081")
